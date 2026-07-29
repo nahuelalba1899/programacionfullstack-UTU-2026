b341b818f2df6f89efa2555bfddae1a316cdcada
nahuelalba1899 <nahuelalba62@gmail.com>
2026-07-29 14:32:53 -0300

    bloque 5 y 6 terminado 7 por arrancar

---
+++ TRABAJOS/php/bloque5.php
@@ -0,0 +1,45 @@
+<?php
+EJERCICIO 1
+//$usuario = "admin";
+//$contrasena = "1234";   
+
+//if ($usuario == "admin" && $contrasena == "1234") {
+  //  echo "Inicio de sesión correcto";
+//} else {
+  //  echo "Usuario o contraseña incorrectos";
+//}
+
+EJERCICIO 2
+
+//$rol = "docente";
+
+//if ($rol == "administrador" || $rol == "docente") {
+  //  echo "Acceso permitido";
+//} else {
+  //  echo "Acceso denegado";
+//}
+EJERCICIO 3
+<?php
+//$precio = 100;              
+//$presupuesto = 500;        
+//$stock = 10;          
+//$cantidadSolicitada = 4; 
+
+//$costoTotal = $precio * $cantidadSolicitada;
+
+//if ($stock >= $cantidadSolicitada && $presupuesto >= $costoTotal) {
+  //  echo "Compra realizada con éxito";
+//} else {
+  //  echo "No se puede realizar la compra";
+//}
+EJERCICIO 4
+<?php
+//$edad = 15;                 
+//$tieneEntrada = true;       
+//$acompañadoPorAdulto = true;
+
+//if (($edad >= 18 && $tieneEntrada) || $acompañadoPorAdulto) {
+  //  echo "Puede entrar";
+//} else {
+  //  echo "No puede entrar";
+//}
---
+++ TRABAJOS/php/bloque6.php
@@ -0,0 +1,69 @@
+<?php
+EJERCICIO 1
+//$edad = 8;  
+//$precio = 200; 
+
+//if ($edad < 12) {
+  //  $precioFinal = $precio * 0.5; 
+//} else {
+  //  $precioFinal = $precio; 
+//}
+//echo "Precio final: " . $precioFinal;
+
+EJERCICIO 2
+
+<?php
+//$precioUnitario = 50;
+//$cantidad = 6;
+
+//$subtotal = $precioUnitario * $cantidad;
+
+//if ($cantidad >= 5) {
+  //  $descuento = $subtotal * 0.10;
+//} else {
+    //$descuento = 0;
+//}
+
+//$totalFinal = $subtotal - $descuento;
+
+//echo "Subtotal: " . $subtotal
+//echo "Descuento: " . $descuento 
+//echo "Total final: " . $totalFinal 
+
+//EJERCICIO   3
+
+<?php
+//$total = 3000; 
+
+//if ($total < 1000) {
+  //  $descuento = 0;
+//} elseif ($total < 5000) {
+  //  $descuento = $total * 0.10; 
+//} else {
+  //  $descuento = $total * 0.20; 
+//}
+
+/   /$totalFinal = $total - $descuento;
+
+//echo "Total original: " . $total . "\n";
+//echo "Descuento: " . $descuento . "\n";
+//echo "Total final: " . $totalFinal . "\n";
+EJERCICIO 4
+<?php
+//$nombreProducto = "Notebook"; 
+//$precio = 1000;                
+//$stock = 20;                   
+//$cantidadSolicitada = 12;     
+
+//if ($cantidadSolicitada > $stock) {
+  //  echo "Error: no hay suficiente stock de " . $nombreProducto;
+//} else {
+  //  $total = $precio * $cantidadSolicitada;
+
+    //if ($cantidadSolicitada >= 10) {
+      //  $total = $total * 0.85; 
+    //}
+
+    //echo "Producto: " . $nombreProducto . "\n";
+    //echo "Total a pagar: " . $total . "\n";
+//}
