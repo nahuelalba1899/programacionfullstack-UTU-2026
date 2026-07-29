2f577c9fd0e7bc06895310ac2e53296879e86858
nahuelalba1899 <nahuelalba62@gmail.com>
2026-07-29 11:18:16 -0300

    bloque 4 trabajo crea hasta el ejercicio 4

---
+++ TRABAJOS/php/BLOQUE4.php
@@ -0,0 +1,47 @@
+<?php
+
+//EJERCICIO 1
+
+
+//$puntos = 65; 
+
+//if ($puntos >= 50) {
+  //  echo "Gano";
+//} else {
+  //  echo "Perdio";
+
+  //EJERCICIO 2
+
+  //  $numero = 8; 
+
+//if ($numero > 0) {
+  //  echo "Positivo";
+//} elseif ($numero < 0) {
+  //  echo "Negativo";
+//} else {
+  //  echo "Cero";
+
+//  EJERCICIO 3
+//$numero = 7;
+
+//if ($numero % 2 == 0) {
+  //  echo "Es par";
+// else {
+  //  echo "Es impar";
+
+
+//EJERCICIO 4
+
+//$nota = 9;
+//if ($nota < 1 || $nota > 12) {
+  //  echo "Error: la nota debe estar entre 1 y 12";
+//} elseif ($nota < 6) {
+  //  echo "Insuficiente";
+//} elseif ($nota <= 8) {
+  //  echo "Aprobado";
+//} elseif ($nota <= 10) {
+  //  echo "Muy bueno";
+//} else {
+  //  echo "Excelente";
+
+
